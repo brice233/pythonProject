4bae397be3026867a0f052333d7d11f61776b9a7
PC <ndahirobrice12@gmail.com>
2024-06-28 01:09:22 -0700

    "study variable"

---
+++ variable.py
@@ -0,0 +1,20 @@
+# variable use to temporarily store data in computer's memory
+price = 10
+print(price)
+
+# challenge 2
+price = 10
+price = 20
+print(price)
+
+# next challenge is boalean value.
+price = 10
+rating = 4.9
+name = 'Mosh'
+is_published = False
+print(price)
+
+# we check in a patient name john smith. he's 20year old and a new patient.
+full_name = 'john smith'
+age = 20
+is_new = True
